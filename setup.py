2c62b91bf7d129660a2ab8b1313db21b76feae8d
bgermann <bgermann@users.noreply.github.com>
2018-06-24 23:47:46 +0200

    Add QtPy dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(  name = 'graphite-graide',
         author = 'M. Hosken',
         package_dir = {'' : 'lib'},
         packages = ['graide', 'graide/freetype', 'graide/makegdl', 'ttfrename'],
-        install_requires = ['future', 'configparser'],
+        install_requires = ['future', 'configparser', 'QtPy'],
         scripts = scripts,
         **kw
 )
