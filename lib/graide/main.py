05cf139c6be43a8f6bafe4cfb6ec5810b7ace944
Sharon Correll <sharon_correll@sil.org>
2018-07-12 16:37:30 -0500

    Fix bug with Graide looking in the wrong directory for the gdlerr.txt file (Graphite engine defaults to where font is output).

--- lib/graide/main.py
+++ lib/graide/main.py
@@ -854,13 +854,14 @@ Copyright 2012-2013 SIL International and M. Hosken""")
             self.tab_tweak.writeXML(self.tweaksfile)
             
         self.tab_errors.clear()        
-        errfile = TemporaryFile(mode="w+")
+        errfile = NamedTemporaryFile(mode="w+")
         
         self.fontFaces = {}
         
         res = buildGraphite(self.config, self, self.font, self.fontFileName, errfile)
         
-        gdlOutputFileName = 'gdlerr.txt'
+        outputPath = os.path.dirname(self.fontFileName)
+        gdlOutputFileName = outputPath + '/gdlerr.txt'
         
         if res :
             # Compilation failure
