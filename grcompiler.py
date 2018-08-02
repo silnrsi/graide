1905f4f737a82a104e69bba53425a0f8f48c7c50
bgermann <bgermann@users.noreply.github.com>
2018-08-02 17:20:06 +0200

    Fix compiling grcompiler on Windows

--- grcompiler.py
+++ grcompiler.py
@@ -5,6 +5,7 @@ except ImportError:
 import os
 import shutil
 import tempfile
+from glob import glob
 from subprocess import check_call
 from zipfile import ZipFile
 from wheel.bdist_wheel import bdist_wheel
@@ -19,6 +20,7 @@ class BuildBdistWheel(bdist_wheel):
         urlretrieve('https://github.com/silnrsi/grcompiler/archive/' + version + '.zip', 'grc.zip')
         grcz = ZipFile('grc.zip')
         grcz.extractall()
+        grcz.close()
 
         os.chdir('grcompiler-' + version)
         if os.name == 'nt':
@@ -26,6 +28,7 @@ class BuildBdistWheel(bdist_wheel):
             urlretrieve('http://download.icu-project.org/files/icu4c/56.1/icu4c-56_1-Win32-msvc10.zip', 'icu.zip')
             icuz = ZipFile('icu.zip')
             icuz.extractall()
+            icuz.close()
 
             # the make file expects an icu source package to find the headers
             os.chdir('icu')
@@ -37,14 +40,15 @@ class BuildBdistWheel(bdist_wheel):
 
             os.chdir('..')
             check_call(['nmake', '-f', 'makefile.mak'])
-            binaries = ['release/*']
+            binaries = glob('release/*')
         else:
             check_call(['autoreconf', '-i'])
             check_call(['./configure'])
             check_call(['make'])
             binaries = ['compiler/grcompiler', 'preprocessor/gdlpp']
         dst = os.path.join(origdir, 'lib', 'graide', 'grcompiler')
-        os.mkdir(dst)
+        if not os.path.exists(dst):
+            os.mkdir(dst)
         for f in binaries:
             shutil.copy(f, dst)
 
