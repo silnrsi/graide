0c3225deadebcd66b1c7c5110de1051a5d800510
bgermann <bgermann@users.noreply.github.com>
2018-08-07 14:30:03 +0200

    Update package name and metadata

---
+++ README.md
@@ -0,0 +1,42 @@
+# GRAIDE: GRAphite Integrate Development Environment
+
+Graide is an integrated development envrionment that can be used
+to develop fonts with Graphite features.
+[Graphite](http://graphite.sil.org) is a "smart font" system
+developed specifically to handle the complexities of lesser-known
+languages of the world.
+
+## Installation
+
+Graide uses [GrCompiler](https://github.com/silnrsi/grcompiler).
+Please install it.
+Alternatively you can build it with this Python package.
+Just set the environment variable `GRCOMPILER_BUNDLE_VERSION`
+to a git reference (e.g. master) and be sure to have its build
+toolchain installed.
+
+You can install graide from from this directory with pip:
+`pip install -e .`
+
+Additionally, run 1 (with Qt4) or 2 (with Qt5):
+1. `pip install PySide`
+2. `pip install PySide2`
+
+You will then have two Python scripts installed: graide and ttfrename.
+
+## Development
+
+The build dependencies are listed in setup.py's `install_requires`.
+Graide uses [QtPy](https://pypi.org/project/QtPy/) to abstract its
+Qt bindings. Therefore [PySide](https://pypi.org/project/PySide/)
+or [PySide2](https://pypi.org/project/PySide2/) is needed as an
+additional runtime dependency. Using any PyQt version is not
+supported.
+
+For those wanting to add images to the lib/graide/images directory.
+Make sure you update the pyresources.qrc and run:
+
+pyside2-rcc -o pyresources.py pyresources.qrc
+sed -i -e 's/PySide2/qtpy/' pyresources.py
+
+so that you can see the images from the program.
--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@
 
 import os
 from setuptools import setup
+from io import open
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 if os.environ.get('GRCOMPILER_BUNDLE_VERSION'):
     import grcompiler
@@ -11,15 +16,28 @@ else:
     BIN = []
     CMDCLASS = {}
 
-setup(  name = 'graphite-graide',
+setup(  name = 'graide',
         version = '0.8',
         description = 'Graphite Integrated Development Environment',
-        author = 'M. Hosken',
+        author = 'Martin Hosken',
+        author_email = 'martin_hosken@sil.org',
+        license = 'LGPL-2.1+',
+        url = 'https://github.com/silnrsi/graide',
         package_dir = {'' : 'lib'},
         packages = ['graide', 'graide/makegdl', 'ttfrename'],
         package_data = {'graide' : BIN},
         install_requires = ['future', 'configparser', 'QtPy', 'fontTools', 'graphite2', 'freetype-py'],
         scripts = ['graide', 'ttfrename'],
         cmdclass = CMDCLASS,
-        zip_safe = False
+        zip_safe = False,
+        long_description = long_description,
+        long_description_content_type = 'text/markdown',
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3'
+        ]
 )
